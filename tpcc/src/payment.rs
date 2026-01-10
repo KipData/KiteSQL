7fdb7d02f8a61b3b778049ce25a702da0590acd3
Kould <kould2333@gmail.com>
2026-01-11 02:31:43 +0800

    Chore/licence (#297)
    * feat: support index cover & fix is_null can't use index
    
    * chore: add unit test for index cover & fix string_encode and string_decode
    
    * chore: add test for composite index trailing columns cover
    
    * chore: fix null_first in sort
    
    * chore: add licence on src files
    
    * chore: add AGENT.md

--- tpcc/src/payment.rs
+++ tpcc/src/payment.rs
@@ -1,3 +1,17 @@
+// Copyright 2024 KipData/KiteSQL
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use crate::load::{last_name, nu_rand, CUST_PER_DIST, DIST_PER_WARE};
 use crate::{other_ware, TpccArgs, TpccError, TpccTest, TpccTransaction, ALLOW_MULTI_WAREHOUSE_TX};
 use chrono::Utc;
